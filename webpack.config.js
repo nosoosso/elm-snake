557a8371d2f3e0dbfea2cb53f4ba3b465d197e88
nosoosso <nosoosso.null@gmail.com>
2017-10-22 00:26:59 +0900

    temp

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,75 @@
 const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+const isProd = process.env.npm_lifecycle_event === 'build' ;
+
+const commonConfig = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist/'),
     filename: '[name]-[hash].js',
   },
+  resolve: {
+    extensions: ['.js', '.elm'],
+    modules: ['node_modules']
+  },
+  module: {
+    noParse: /\.elm$/,
+    rules: [
+      {
+      }
+    ]
+  }
 }
+
+let config;
+if (isProd) {
+  config = merge(commonConfig, {
+    module: {
+      rules: [{
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: 'elm-webpack-loader'
+      }, {
+        test: /\.sc?ss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }]
+    },
+    plugins: [
+
+      new ExtractTextPlugin({
+        filename: 'static/css/[name]-[hash].css',
+        allChunks: true,
+      }),
+      new CopyWebpackPlugin([{
+        from: 'src/static/img/',
+        to: 'static/img/'
+      }, {
+        from: 'src/favicon.ico'
+      }]),
+
+      // extract CSS into a separate file
+      // minify & mangle JS/CSS
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compressor: {
+          warnings: false
+        }
+        // mangle:  true
+      })
+    ]
+  });
+} else {
+  config = merge(commonConfig, {
+
+  });
+}
+
+module.exports = config;
